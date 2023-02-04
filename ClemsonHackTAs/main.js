9a4cc635c81b44c97fde11d913d25a408d4a1fe3
Tim-Tim-Tim <timgs2002@gmail.com>
2023-02-04 11:52:48 -0500

    piece number 3

--- ClemsonHackTAs/main.js
+++ ClemsonHackTAs/main.js
@@ -4,3 +4,10 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 500);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerHeight,window.innerHeight);
