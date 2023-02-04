212f0af154623c66c9a4f17e2ca87d9fe30ea142
Tim-Tim-Tim <timgs2002@gmail.com>
2023-02-04 12:18:38 -0500

    part 5

--- ClemsonHackTAs/main.js
+++ ClemsonHackTAs/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three';
+import { WireframeGeometry } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 500);
@@ -10,4 +11,26 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerHeight,window.innerHeight);
+renderer.setSize(window.innerWidth,window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene,camera);
+
+const geometry = new THREE.IcosahedronGeometry(6,10);
+const material = new THREE.MeshBasicMaterial({color: 0xFA3135,wireframe: true});
+const icosahedron = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  icosahedron.rotation.x += 0.01;
+  icosahedron.rotation.z += 0.01;
+  icosahedron.rotation.y += 0.005;
+
+
+  renderer.render(scene, camera);
+}
+
+animate();
